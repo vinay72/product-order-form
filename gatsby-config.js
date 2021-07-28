72fee3e85db9d1b1a7fd66595cb0ecbf39cb1cbd
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2021-07-28 02:58:38 -0700

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  pathPrefix: '/product-order-form',
+  siteMetadata: {
+    title: `Product Order Form`,
+    description: `A simple product validation with Formik hosted in Netlify with Netlify Forms`,
+    author: `Vinay Sagar Sharma`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-theme-material-ui`,
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      options: {
+        stylesProvider: {
+          injectFirst: true,
+        },
+      },
+    },
+    `gatsby-plugin-styled-components`,
+    // `gatsby-plugin-offline`,
+  ],
+};
